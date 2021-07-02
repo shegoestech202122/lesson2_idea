ad5fe136ff271d567a0f0093fb65e980533e9752
shegoestech202122 <86735273+shegoestech202122@users.noreply.github.com>
2021-07-02 19:00:57 +0300

    Update NewApp.java

--- src/NewApp.java
+++ src/NewApp.java
@@ -1,7 +1,6 @@
 public class NewApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, students");
-        System.out.println("Hello, from Artjoms");
+        System.out.println("Hello from venus");
     }
 }
