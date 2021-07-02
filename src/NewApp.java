248109a035979385eac8d8723c9c24865f5cbe25
Artjoms <artjoms.v.petrovs@gmail.com>
2021-07-02 19:07:36 +0300

    Update NewApp.java

--- src/NewApp.java
+++ src/NewApp.java
@@ -1,6 +1,6 @@
 public class NewApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello from venus");
+        System.out.println("Hello , hello, hello!");
     }
 }
